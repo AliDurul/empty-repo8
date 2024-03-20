36472f51b120a612d405dd16da1060b211786ed3
Ali <alidrl26@gmail.com>
2024-03-20 10:41:43 +0200

    hero animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import styles from "../style";
 import { discount, robot, robot1 } from "../assets";
 import GetStarted from "./GetStarted";
+import { useEffect, useState } from "react";
 
 const Hero = () => {
 
@@ -13,19 +14,34 @@ const Hero = () => {
     });
   });
 
+  const [text, setText] = useState('Loading...');
+
+  useEffect(() => {
+    // Set a timeout to change the text after 3 seconds
+    const timer = setTimeout(() => {
+      setText('Loaded 100');
+    }, 2600);
+
+    // Cleanup function to clear the timeout if the component unmounts before the timeout is reached
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
-          <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
-          <span className="text-white">100</span> {" "}
+          {/* <img src={discount} alt="discount" className="w-[32px] h-[32px]" /> */}
+          {/* <span className="text-white"></span> {" "} */}
+          {
+            text === 'Loading...' && <div className="progress-6"></div>
+          }
 
           <p className={`${styles.paragraph} ml-2`}>
-            {/* <span className="text-white">100</span> {" "} */}
-            <span className="text-white">Loaded. </span>
+            <span className="text-white">{text}</span> {" "}
           </p>
-          {/* <div className="progress-6"></div> */}
-          {/* <div id="deploymentMessage" className="disabled:">Software Deployed</div> */}
+          {
+            text === 'Loaded 100' && <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
+          }
         </div>
 
         <div className="flex flex-row justify-between items-center w-full">
