974c16d3ea1c04a3fe65197831faa4fa3ddb0a20
AliDurul <alidrl26@gmail.com>
2024-12-27 12:36:34 +0200

    remove unused files and components from LandingPage

--- LandingPage/src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Button = ({ styles }) => (
-  <button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`} onClick={() => document.getElementById('my_modal_3').showModal()}>
-    Get Started
-  </button>
-);
-
-export default Button;
